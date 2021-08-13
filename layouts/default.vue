da80b40788c9dfc5b2bf26b38a294bafc4300744
nhtuan1609 <nhtuan1609@gmail.com>
2021-08-14 00:01:53 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app>
+    <v-app-bar fixed app>
+      <nuxt-link to="/" class="d-flex align-center">
+        <NuxtLogo />
+      </nuxt-link>
+
+      <v-row justify="end" class="ma-0 ml-1">
+        <nuxt-link to="/Snake" class="nav-link">Snake</nuxt-link>
+        <nuxt-link to="/" class="nav-link">Home</nuxt-link>
+      </v-row>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid class="pa-0 ma-0">
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer app>
+      <span>&copy; Nguyen Hoang Tuan</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.nav-link {
+  text-decoration: none;
+  font-size: 1.4rem;
+  font-weight: 600;
+  color: rgb(95, 95, 95) !important;
+  padding: 0 10px;
+  margin: 0 10px;
+}
+.nav-link:hover {
+  color: #f9a825 !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div
+      class="
+        display-3
+        text-center
+        font-weight-medium
+        yellow--text
+        text--darken-3
+        my-10
+      "
+    >
+      Hello world, I'm Vue
+    </div>
+    <div class="d-flex justify-center mt-16">
+      <nuxt-link to="/snake" class="mx-10" style="text-decoration: none">
+        <v-card width="400" height="400">
+          <v-img src="img/snake.png"></v-img>
+        </v-card>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
